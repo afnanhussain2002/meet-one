9471de6065d9fa60ea74b3939529bea02d3e433c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-10 16:57:39 +0600

    add code of sheet

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  
 
 const MobileNav = () => {
   return (
    <section className='w-full max-w-[264px]'>
+<Sheet>
+  <SheetTrigger>Open</SheetTrigger>
+  <SheetContent>
+    <SheetHeader>
+      <SheetTitle>Are you absolutely sure?</SheetTitle>
+      <SheetDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
 
    </section>
   )
