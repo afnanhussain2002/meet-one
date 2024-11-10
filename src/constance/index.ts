139717e77f32628b24cdb02e9ea3583a75611787
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-10 07:49:48 +0600

    write the menu

---
+++ src/constance/index.ts
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+    {
+        label: "Home",
+        route: "/",
+        imgUrl: "/icons/home.svg",
+    },
+    {
+        label: "Upcoming",
+        route: "/upcoming",
+        imgUrl: "/icons/upcoming.svg",
+    },
+    {
+        label: "Previous",
+        route: "/previous",
+        imgUrl: "/icons/previous.svg",
+    },
+    {
+        label: "Recordings",
+        route: "/recordings",
+        imgUrl: "/icons/recordings.svg",
+    },
+]
