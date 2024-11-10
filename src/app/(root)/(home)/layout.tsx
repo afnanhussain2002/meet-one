1d75d3cd2c18c944c27ed09a4ca94514da381bc1
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-10 06:53:30 +0600

    write some design for home layout

---
+++ src/app/(root)/(home)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="relative">
+      Navbar
+      <div className="flex">Sidebar</div>
+      <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14"></section>
+      {children}
+    </main>
+  );
+};
+
+export default HomeLayout;
