092498f235449289587d8ed77b1240dab0b519d4
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-11 09:14:05 +0600

    make a sign in button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,34 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import logo from '@/public/icons/logo.svg'
-import MobileNav from './MobileNav'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "@/public/icons/logo.svg";
+import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
-      <Link
-      href={'/'}
-      className='flex items-center gap-1'
-      >
-    <Image
-    src={logo}
-    width={32}
-    height={32}
-    alt='MeetOne Logo'
-    className='max-sm:size-10'
-    />
-    <p className='text-[26px] font-extrabold text-white max-sm:hidden'>MeetOne</p>
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href={"/"} className="flex items-center gap-1">
+        <Image
+          src={logo}
+          width={32}
+          height={32}
+          alt="MeetOne Logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          MeetOne
+        </p>
       </Link>
 
-      <div className='flex-between gap-5'>
-      <MobileNav/>
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <MobileNav />
       </div>
-
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
