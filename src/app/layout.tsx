a664bf994ca6236c04653f4e5298d906e44ab0f7
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-11 17:04:28 +0600

    change social share button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import logo from "../public/icons/logo.svg";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,6 +28,10 @@ export default function RootLayout({
   return (
     <ClerkProvider
     appearance={{
+      layout:{
+        logoImageUrl: "../public/icons/logo.svg",
+        socialButtonsVariant:'iconButton'
+      },
       variables: {
         colorText: "#fff",
         colorPrimary:"#0e78f9",
