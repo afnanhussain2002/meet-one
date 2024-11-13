bc5886f741b0b15ab669955b3e3ed838cb83cf7e
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-13 15:29:11 +0600

    get children as prop and define its type

--- src/providres/StreamClientProvider.tsx
+++ src/providres/StreamClientProvider.tsx
@@ -1,11 +1,12 @@
 import {
     StreamVideo,
   } from '@stream-io/video-react-sdk';
+import { ReactNode } from 'react';
   
   const apiKey = process.env.NEXT_PUBLIC_STREAM_KEY;
  
   
- const StreamVideoProvider = () => {
+ const StreamVideoProvider = ({children}:{children: ReactNode}) => {
     return (
       <StreamVideo client={client}>
      
