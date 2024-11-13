758e50ae259d3edfa319063d12d4d6571f1ac0b5
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-13 11:33:24 +0600

    npx shadcn@latest add dialog

--- src/components/MeetingModal.tsx
+++ src/components/MeetingModal.tsx
@@ -1,4 +1,18 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
+
+interface MeetingModalProps {
+    isOpen: boolean;
+    onClose: () => void;
+    title: string;
+    className?: string;
+    children?: ReactNode;
+    handleClick?: () => void;
+    buttonText?: string;
+    instantMeeting?: boolean;
+    image?: string;
+    buttonClassName?: string;
+    buttonIcon?: string;
+  }
 
 const MeetingModal = ({isOpen, onClose, children, handleClink, buttonText, title, image, buttonIcon }: MeetingModalProps) => {
   return (
