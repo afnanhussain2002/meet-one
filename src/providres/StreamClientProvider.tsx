585bac48790dbc00e5a24f505a1d3d87f12adb24
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-13 15:31:08 +0600

    define a video client state

--- src/providres/StreamClientProvider.tsx
+++ src/providres/StreamClientProvider.tsx
@@ -1,14 +1,17 @@
 import {
     StreamVideo,
+    StreamVideoClient,
   } from '@stream-io/video-react-sdk';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
   
   const apiKey = process.env.NEXT_PUBLIC_STREAM_KEY;
  
   
  const StreamVideoProvider = ({children}:{children: ReactNode}) => {
+
+    const [videoClient, setVideoClient] = useState<StreamVideoClient>();
     return (
-      <StreamVideo client={client}>
+      <StreamVideo client={videoClient}>
      
       </StreamVideo>
     );
