83a5f9a524963748dd23c56fd1b930bf125d8e8d
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-13 10:30:57 +0600

    import and show the home card

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -1,11 +1,12 @@
 "use client"
 import React from 'react'
+import HomeCard from './HomeCard'
 
 
 const MeetingTypeList = () => {
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
-     
+     <HomeCard/>
     </section>
   )
 }
