211258fe6492d3969271d4527510e9ce17517367
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-13 10:24:57 +0600

    make a file for home card

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -1,12 +1,12 @@
 "use client"
 import React from 'react'
+import AddMeeting from '@/public/icons/add-meeting.svg'
+import Image from 'next/image'
 
 const MeetingTypeList = () => {
   return (
     <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
-     <div className='bg-orange-1 px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer' onClick={() => {}}>
-   box 1
-     </div>
+     
     </section>
   )
 }
