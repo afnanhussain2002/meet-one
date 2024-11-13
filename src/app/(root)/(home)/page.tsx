47af23c492e0733d23a113994ff11a6981728d10
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-13 09:21:50 +0600

    write a text on home page

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -5,7 +5,9 @@ const Home = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
         <div className="h-[300px] w-full rounded-[20px] bg-[url('https://i.ibb.co.com/JCCqmVM/laptop-with-glowing-screen-table-dark-top-view-copy-space.jpg')] bg-cover">
-
+     <div className='flex h-full flex-col justify-between max-md:px-5 max-md:pt-8 lg:p-11 '>
+    <h2 className='glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal'>Upcoming Meeting at: 12:30 PM</h2>
+     </div>
         </div>
 
     </section>
