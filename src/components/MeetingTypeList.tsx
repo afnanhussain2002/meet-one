31e6d6ef016a6adc8d4ac14ff4bde896519a481c
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-13 10:36:45 +0600

    make card components perfect for re useable

--- src/components/MeetingTypeList.tsx
+++ src/components/MeetingTypeList.tsx
@@ -1,14 +1,15 @@
-"use client"
-import React from 'react'
-import HomeCard from './HomeCard'
-
+"use client";
+import React, { useState } from "react";
+import HomeCard from "./HomeCard";
+import AddMeeting from '@/public/icons/add-meeting.svg'
 
 const MeetingTypeList = () => {
+    const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isInstantMeeting' | 'isJoiningMeeting' | undefined>();
   return (
-    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
-     <HomeCard/>
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard img={AddMeeting} title="New Meeting" description="Start an instant meeting" handleClink={() => setMeetingState('isJoiningMeeting')}/>
     </section>
-  )
-}
+  );
+};
 
-export default MeetingTypeList
+export default MeetingTypeList;
