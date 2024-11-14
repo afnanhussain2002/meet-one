a73e8a2e4b51a41b86b6ca970a4ff92fedacad59
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-14 21:37:48 +0600

    define a state for video toggle

--- src/components/MeetingSetup.tsx
+++ src/components/MeetingSetup.tsx
@@ -1,7 +1,8 @@
 import { VideoPreview } from '@stream-io/video-react-sdk'
-import React from 'react'
+import React, { useState } from 'react'
 
 const MeetingSetup = () => {
+    const [isMicCamToggledOn, setisMicCamToggledOn] = useState(false)
   return (
     <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
 
