c46645dba89610d30c7c2fa59fa866662860d70d
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-14 21:29:57 +0600

    get the call and call loading

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -1,14 +1,16 @@
 "use client";
 import MeetingRoom from "@/components/MeetingRoom";
 import MeetingSetup from "@/components/MeetingSetup";
+import { useGetCallById } from "@/hooks/useGetCallById";
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
 
-const Meeting = ({ params }: { params: { id: string } }) => {
+const Meeting = ({ params: {id} }: { params: { id: string } }) => {
   const { user, isLoaded } = useUser();
 
   const [isSetupComplete, setisSetupComplete] = useState(false);
+  const {call, isCallLoading} = useGetCallById(id)
   return (
     <main className="h-screen w-full">
       <StreamCall>
