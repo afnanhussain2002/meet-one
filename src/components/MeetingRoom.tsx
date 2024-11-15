141c7941abec939d73847d9d132decbd32fda8d9
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-15 12:11:05 +0600

    npx shadcn@latest add dropdown-menu

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,9 +1,11 @@
-import { PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
+import { cn } from '@/lib/utils'
+import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
 import React, { useState } from 'react'
 
 type CallLayoutType = 'speaker-left' | 'speaker-right' | 'grid'
 const MeetingRoom = () => {
   const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
+  const [showPerticipents, setShowPerticipents] = useState(false)
 
   const CallLayout = () => {
     switch (layout) {
@@ -21,6 +23,14 @@ const MeetingRoom = () => {
    <div className='flex size-full max-w-[1000px] items-center'>
   <CallLayout/>
    </div>
+<div className={cn('h-[calc(100vh-86px)] hidden ml-2',{'show-block': showPerticipents})}>
+<CallParticipantsList onClose={() => setShowPerticipents(false)}/>
+</div>
+   </div>
+   <div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
+
+    <CallControls/>
+
    </div>
     </section>
   )
