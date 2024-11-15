0251efd30830e006dbef63623ce967580a90594a
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-15 07:29:47 +0600

    define a use state hook

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MeetingRoom = () => {
+  const [layout, setLayout] = useState('speaker-left')
   return (
     <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
+   <div className='relative flex size-full items-center justify-center'>
+   <div className='flex size-full max-w-[1000px] items-center'>
 
+   </div>
+   </div>
     </section>
   )
 }
