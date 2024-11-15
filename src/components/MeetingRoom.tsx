88ffec1b71d0e374b2b16303c9b2116bc9ca67e0
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-15 07:26:37 +0600

    creating the layout of meeting room

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const MeetingRoom = () => {
   return (
-    <div>MeetingRoom</div>
+    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
+
+    </section>
   )
 }
 
