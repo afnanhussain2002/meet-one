e38491152317359d89ca6575c5490e8f5781ab58
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-15 15:01:13 +0600

    define button component for end call for everyone

--- src/components/EndCallButton.tsx
+++ src/components/EndCallButton.tsx
@@ -1,18 +1,32 @@
-"use client"
-import { useCall, useCallStateHooks } from '@stream-io/video-react-sdk';
-import React from 'react'
+"use client";
+import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
+import React from "react";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 const EndCallButton = () => {
-    const call = useCall();
-    const {useLocalParticipant} = useCallStateHooks();
-    const localParticipant = useLocalParticipant();
+  const router = useRouter();
+  const call = useCall();
+  const { useLocalParticipant } = useCallStateHooks();
+  const localParticipant = useLocalParticipant();
 
-    const isMeetingOwner = localParticipant && call?.state.createdBy && localParticipant.userId === call?.state.createdBy.id;
+  const isMeetingOwner =
+    localParticipant &&
+    call?.state.createdBy &&
+    localParticipant.userId === call?.state.createdBy.id;
 
-    if(!isMeetingOwner) return null;
+  if (!isMeetingOwner) return null;
   return (
-    <div>EndCallButton</div>
-  )
-}
+    <Button
+      onClick={async () => {
+        await call.endCall();
+        router.push("/");
+      }}
+      className="bg-red-500"  
+    >
+        End Call For Everyone
+    </Button>
+  );
+};
 
-export default EndCallButton
+export default EndCallButton;
