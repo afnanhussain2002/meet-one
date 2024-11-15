12cc211b2d3b38ddcb340dad30bf3ed8c1b1f6cf
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-15 07:35:38 +0600

    define a switch case on a func

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,7 +1,20 @@
+import { PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
 import React, { useState } from 'react'
 
+type CallLayoutType = 'speaker-left' | 'speaker-right' | 'grid'
 const MeetingRoom = () => {
-  const [layout, setLayout] = useState('speaker-left')
+  const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
+
+  const CallLayout = () => {
+    switch (layout) {
+      case 'grid':
+        return <PaginatedGridLayout/>
+      case 'speaker-right':
+        return <SpeakerLayout participantsBarPosition={'left'}/>  
+      default:
+        return <SpeakerLayout participantsBarPosition={'right'} />
+    }
+  }
   return (
     <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
    <div className='relative flex size-full items-center justify-center'>
