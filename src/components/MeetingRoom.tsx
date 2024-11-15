e93a4fec6c3cc6d2263a6e212baa5487e73c029e
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-15 12:18:09 +0600

    add layout list and change the size

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -1,6 +1,11 @@
-import { cn } from '@/lib/utils'
-import { CallControls, CallParticipantsList, PaginatedGridLayout, SpeakerLayout } from '@stream-io/video-react-sdk'
-import React, { useState } from 'react'
+import { cn } from "@/lib/utils";
+import {
+  CallControls,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,41 +13,61 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
+import { LayoutList } from "lucide-react";
 
-
-type CallLayoutType = 'speaker-left' | 'speaker-right' | 'grid'
+type CallLayoutType = "speaker-left" | "speaker-right" | "grid";
 const MeetingRoom = () => {
-  const [layout, setLayout] = useState<CallLayoutType>('speaker-left')
-  const [showPerticipents, setShowPerticipents] = useState(false)
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showPerticipents, setShowPerticipents] = useState(false);
 
   const CallLayout = () => {
     switch (layout) {
-      case 'grid':
-        return <PaginatedGridLayout/>
-      case 'speaker-right':
-        return <SpeakerLayout participantsBarPosition={'left'}/>  
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition={"left"} />;
       default:
-        return <SpeakerLayout participantsBarPosition={'right'} />
+        return <SpeakerLayout participantsBarPosition={"right"} />;
     }
-  }
+  };
   return (
-    <section className='relative h-screen w-full overflow-hidden pt-4 text-white'>
-   <div className='relative flex size-full items-center justify-center'>
-   <div className='flex size-full max-w-[1000px] items-center'>
-  <CallLayout/>
-   </div>
-<div className={cn('h-[calc(100vh-86px)] hidden ml-2',{'show-block': showPerticipents})}>
-<CallParticipantsList onClose={() => setShowPerticipents(false)}/>
-</div>
-   </div>
-   <div className='fixed bottom-0 flex w-full items-center justify-center gap-5'>
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayout />
+        </div>
+        <div
+          className={cn("h-[calc(100vh-86px)] hidden ml-2", {
+            "show-block": showPerticipents,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowPerticipents(false)} />
+        </div>
+      </div>
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls />
 
-    <CallControls/>
+        <DropdownMenu>
+          <div className="flex items-center">
 
-   </div>
+          <DropdownMenuTrigger>
+      <LayoutList size={20} className="text-white"/>
+
+          </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default MeetingRoom
+export default MeetingRoom;
