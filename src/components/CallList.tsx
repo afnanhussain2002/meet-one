e89a2fc0441eeeb630348bda7cb759fee2e5d916
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-16 11:33:04 +0600

    add the call type on switch case

--- src/components/CallList.tsx
+++ src/components/CallList.tsx
@@ -1,9 +1,28 @@
 'use client'
 import { useGetCalls } from '@/hooks/useGetCalls'
-import React from 'react'
+import { CallRecording } from '@stream-io/node-sdk'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 
 const CallList = ({type}: {type: 'ended' | 'upcoming' | 'recordings'}) => {
   const {endedCalls, upcomingCalls, callRecordings, isLoading} = useGetCalls()
+
+  const router = useRouter(); 
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+  const getCalls = () => {
+    switch (type) {
+      case "ended":
+        return endedCalls;
+      case "recordings":
+        
+      return callRecordings;
+      case "upcoming":
+        return upcomingCalls;       
+      default:
+        return[]
+    }
+  }
   return (
     <div>CallList</div>
   )
