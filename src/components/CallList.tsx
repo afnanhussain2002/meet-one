83a370fd9c7eee8049cd046e281cae1a873cf031
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-16 07:23:14 +0600

    import use get calls hook

--- src/components/CallList.tsx
+++ src/components/CallList.tsx
@@ -1,6 +1,8 @@
+import { useGetCalls } from '@/hooks/useGetCalls'
 import React from 'react'
 
 const CallList = ({type}: {type: 'ended' | 'upcoming' | 'recordings'}) => {
+  const {endedCalls, upcomingCalls, callRecordings, isLoading} = useGetCalls()
   return (
     <div>CallList</div>
   )
