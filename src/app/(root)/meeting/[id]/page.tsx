570c8914876bef2161fa8e5f598e708ed2fd8b47
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-17 21:04:28 +0600

    remove user from meeting page

--- src/app/(root)/meeting/[id]/page.tsx
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -8,7 +8,7 @@ import { Loader } from "lucide-react";
 import React, { useState } from "react";
 
 const Meeting =  ({ params: {id} }: { params: { id: string } }) => {
-  const { user, isLoaded } = useUser();
+  const { isLoaded } = useUser();
 
   const [isSetupComplete, setIsSetupComplete] = useState(false);
   const {call, isCallLoading} = useGetCallById(id)
